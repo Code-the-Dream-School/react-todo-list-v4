030052cab2968d31ecd8c5c938cecc69e6f4c276
John McGarvey <johnmcgarvey55@gmail.com>
2026-01-08 17:02:30 -0500

    create config for use in vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3001 },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const backend = env.VITE_BACK_END;
+  const isHttps = backend?.startsWith('https://');
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3001,
+      proxy: {
+        '/remote-api': {
+          target: backend,
+          changeOrigin: true,
+          secure: isHttps,
+          rewrite: (p) => p.replace(/^\/remote-api/, ''),
+          configure: (proxy) => {
+            proxy.on('proxyReq', (proxyReq) => {
+              // Manually set the Origin header to match your target
+              proxyReq.setHeader('Origin', backend);
+            });
+          },
+        },
+      },
+    },
+  });
+};
