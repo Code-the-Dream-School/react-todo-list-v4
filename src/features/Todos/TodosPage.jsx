f721defadc9b116bc4fe26e826e4947b03d2fb39
Roy Mosby <ego.madking@gmail.com>
2026-03-05 10:25:32 -0500

    chore: update API endpoints to use relative paths

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -11,8 +11,6 @@ import {
 } from '../../reducers/todoReducer';
 import { useAuth } from '../../contexts/AuthContext';
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
-
 function TodosPage() {
   const { token } = useAuth();
   const [state, dispatch] = useReducer(todoReducer, initialTodoState);
@@ -47,7 +45,7 @@ function TodosPage() {
         credentials: 'include',
       };
       try {
-        const resp = await fetch(`${baseUrl}/tasks?${params}`, options);
+        const resp = await fetch(`/api/tasks?${params}`, options);
         if (resp.status === 401) {
           throw new Error('unauthorized');
         }
@@ -108,7 +106,7 @@ function TodosPage() {
         }),
         credentials: 'include',
       };
-      const resp = await fetch(`${baseUrl}/tasks`, options);
+      const resp = await fetch('/api/tasks', options);
       if (!resp.ok) {
         throw new Error(resp.message || 'Failed to add todo');
       }
@@ -154,7 +152,7 @@ function TodosPage() {
         }),
         credentials: 'include',
       };
-      const resp = await fetch(`${baseUrl}/tasks/${id}`, options);
+      const resp = await fetch(`/api/tasks/${id}`, options);
       if (!resp.ok) {
         throw new Error(resp.message || 'Failed to complete todo');
       }
@@ -196,7 +194,7 @@ function TodosPage() {
         }),
         credentials: 'include',
       };
-      const resp = await fetch(`${baseUrl}/tasks/${editedTodo.id}`, options);
+      const resp = await fetch(`/api/tasks/${editedTodo.id}`, options);
       if (!resp.ok) {
         throw new Error(resp.message || 'Failed to update todo');
       }
